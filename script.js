19493fcbc5105ddbd558dcaa1974564a41dfc92b
sobrowine <sbwineandliquor@gmail.com>
2025-12-26 22:46:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const sheetID = "https://docs.google.com/spreadsheets/d/10AKTBh0YJ2S0jtM3UNEljvJr0PX1g2SNHOd3j2xtbJ0/edit";
+const sheetID = "https://docs.google.com/spreadsheets/d/10AKTBh0YJ2S0jtM3UNEljvJr0PX1g2SNHOd3j2xtbJ0";
 const sheetName = "Sheet1";
 
 const url = `https://opensheet.elk.sh/${sheetID}/${sheetName}`;
